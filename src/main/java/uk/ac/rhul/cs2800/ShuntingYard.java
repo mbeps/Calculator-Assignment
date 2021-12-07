10d59d8be23ad073fa93f40b71e7cf1b33ba56f9
zjac268 <zjac268@0d3bd814-0b19-4016-a467-482d2ac05296>
2021-12-07 12:52:17 +0000

    Shunting Yard algorithm implemeted - Tests fully implemented - Algorithm passes tests and therefore is operational - Documentation added

--- src/main/java/uk/ac/rhul/cs2800/ShuntingYard.java
+++ src/main/java/uk/ac/rhul/cs2800/ShuntingYard.java
@@ -1,24 +1,88 @@
 package uk.ac.rhul.cs2800;
 
+import java.lang.Character;
+
 public class ShuntingYard {
-    /**
-     * Orders sign by precedence. 
-     * Higher precedence returns higher value. 
-     * @param sign (char): sign to be tested for precedence
-     * @return (int): precedence of the sign
-     */
-    public static int getPrecedence(char sign) {
-        if (sign == '+' || sign == '-') {
-            return 1;
-        } else if (sign == '*' || sign == '/') {
-            return 2;
-        } else if (sign == '^') {
-            return 3;
+  private static Stack<Character> stack;
+
+  /**
+   * Check whether character passed is a letter or digit. 
+   * Symbols do count as letter or digit. 
+   * 
+   * @param character (char): character to be checked
+   * @return (boolean): whether character is letter / digit
+   * @author Maruf
+   */
+  static boolean isLetterOrDigit(char character) {
+    return Character.isLetterOrDigit(character);
+  }
+
+  /**
+   * Orders sign by precedence.
+   * Higher precedence returns higher value.
+   * 
+   * @param sign (char): sign to be tested for precedence
+   * @return (int): precedence of the sign
+   * @author Maruf
+   */
+  public static int getPrecedence(char sign) {
+    if (sign == '+' || sign == '-') {
+      return 1;
+    } else if (sign == '*' || sign == '/') {
+      return 2;
+    } else if (sign == '^') {
+      return 3;
+    }
+    return -1;
+  }
+
+  /**
+   * Converts infix notation to postfix notation
+   * 
+   * @param expression (String): expression to be converted
+   * @return (String): converted postfix expression
+   * @author Maruf
+   */
+  public static String converter(String expression) {
+    stack = new Stack<>();
+    String output = "";
+
+    // Iterate over tokens in the expression
+    for (int i = 0; i < expression.length(); ++i) {
+      char characterAtIndex = expression.charAt(i); // I-th character
+      // Operand added to output
+      if (isLetterOrDigit(characterAtIndex)) {
+        output += characterAtIndex;
+      }
+      // Push '(' to stack
+      else if (characterAtIndex == '(') {
+        stack.push(characterAtIndex);
+      }
+      // Pop ')' and append to output from stack until '('
+      else if (characterAtIndex == ')') {
+        // Only when the stack is not empty and the last element is not (
+        while (!stack.isEmpty() && stack.top() != '(') {
+          output += stack.top();
+          stack.pop();
         }
-        return -1;
+        stack.pop();
+      } else { // Operands
+        while (!stack.isEmpty() && getPrecedence(characterAtIndex) <= getPrecedence(stack.top())) {
+          output += stack.top();
+          stack.pop();
+        }
+        stack.push(characterAtIndex);
+      }
     }
 
-    public static void main(String[] args) {
-        System.out.println(getPrecedence('+'));
+    // Pop all the remaining operators from the stack and append them to output
+    while (!stack.isEmpty()) {
+      if (stack.top() == '(') {
+        return "Invalid Expression";
+      }
+      output += stack.top();
+      stack.pop();
     }
+    return output;
+  }
 }
