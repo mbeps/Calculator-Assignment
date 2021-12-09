9cfc4e92e917153d5febc06fbf9225f934eabc5b
zjac268 <zjac268@0d3bd814-0b19-4016-a467-482d2ac05296>
2021-12-09 19:13:28 +0000

    Evaluation functional given stack - Documentation added to class - Better tests created to find problem - Converting expression into stack not yet implemented - Infix notation no longer required as Shunting Yard algorthm can be used to convert infix to postfix hence reuse this algorithm

--- src/test/java/uk/ac/rhul/cs2800/TestPostfixEvaluation.java
+++ src/test/java/uk/ac/rhul/cs2800/TestPostfixEvaluation.java
@@ -4,9 +4,73 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 public class TestPostfixEvaluation {
+    PostfixEvaluation foo;
+    @BeforeEach
+    void setup() {
+        foo = new PostfixEvaluation();
+    }
+    
     @Test
-    void testEvaluate() {
-        PostfixEvaluation foo = new PostfixEvaluation();
+    void testEvaluateAddition() { // Test 2
+        /**
+         * 1 - Failed as index position was miscalculated 
+         * 2 - Index position fixed (pass)
+         */
+        foo.pushOperand(5);
+        foo.pushOperand(4);
+
+        foo.pushOperator("+");
+
+        assertEquals(9, foo.evaluate());
+    }
+
+    @Test
+    void testEvaluateSubtraction() { // Test 3
+        /**
+         * 1 - Failed as index position was miscalculated 
+         * 2 - Index position fixed (pass)
+         */
+        foo.pushOperand(3);
+        foo.pushOperand(8);
+
+        foo.pushOperator("-");
+
+        assertEquals(5, foo.evaluate());
+    }
+    @Test
+    void testEvaluateMultiplication() { // Test 4
+        /**
+         * 1 - Failed as index position was miscalculated 
+         * 2 - Index position fixed (pass)
+         */
+        foo.pushOperand(5);
+        foo.pushOperand(4);
+
+        foo.pushOperator("*");
+
+        assertEquals(20, foo.evaluate());
+    }
+
+    @Test
+    void testEvaluateDivision() { // Test 5
+        /**
+         * 1 - Failed as index position was miscalculated 
+         * 2 - Index position fixed (pass)
+         */
+        foo.pushOperand(4);
+        foo.pushOperand(8);
+
+        foo.pushOperator("/");
+
+        assertEquals(2, foo.evaluate());
+    }
+    
+    @Test
+    void testEvaluate() { // Test 1
+        /**
+         * 1 - Full algorithm required for testing (fail)
+         * 2 - Other smaller tests written to find fault (pass)
+         */
         foo.pushOperand(5);
         foo.pushOperand(4);
         foo.pushOperand(2);
@@ -15,18 +79,5 @@ public class TestPostfixEvaluation {
         foo.pushOperator("+");
 
         assertEquals(13, foo.evaluate());
-    }
-
-    @Test
-    void testEvaluate2() {
-        PostfixEvaluation foo = new PostfixEvaluation();
-        foo.pushOperand(7);
-        foo.pushOperand(6);
-        foo.pushOperand(3);
-
-        foo.pushOperator("/");
-        foo.pushOperator("-");
-
-        assertEquals(5, foo.evaluate());
     }
 }
